e8f3177ee4a47e72599c5a2c71d010a36a4bd9bd
imithu <imithu@imithu.org>
2021-11-07 10:59:27 +0600

    🗑️ update - deprecate datetime

--- Misc/Moment.php
+++ Misc/Moment.php
@@ -5,18 +5,24 @@ namespace Misc;
 
 class Moment
 {
+
+
   /**
+   * @deprecated
+   * 
    * get current utc datetime
    * 
    * @return  string utc datetime  eg. 2021-05-03 14:51:08
    * 
-   * @since   1.0.0
-   * @version 1.0.0
-   * @author  Mahmudul Hasan Mithu
+   * @since   🌱 1.0.0
+   * @version 🌴 1.3.0
+   * @author  ✍ Muhammad Mahmudul Hasan Mithu
    */
   public static function datetime()
   {
     date_default_timezone_set('UTC');
     return date('Y-m-d H:i:s');
   }
+
+
 }
